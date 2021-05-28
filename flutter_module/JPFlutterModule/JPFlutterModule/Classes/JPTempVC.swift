4776e25cc7302c1ce582353970675f71540ee155
zhoujianping <zhoujianping@52tt.com>
2021-05-28 10:57:35 +0800

    更新注释

--- flutter_module/JPFlutterModule/JPFlutterModule/Classes/JPTempVC.swift
+++ flutter_module/JPFlutterModule/JPFlutterModule/Classes/JPTempVC.swift
@@ -57,14 +57,15 @@ class JPTempVC: UIViewController, UIGestureRecognizerDelegate {
     @objc func close() {
         dismiss(animated: true, completion: {
             /*
-             * 【dismiss方法会内存泄漏！！！！！！】
-             * Flutter页面控制器 是FLBFlutterViewContainer
-             * FLBFlutterViewContainer 内部要调用 notifyWillDealloc 才能真正的死去
-             * 而 notifyWillDealloc 是内部方法，只在 dismiss 和 didMove(toParent: nil) 方法里面调用
-             * 所以这里只有这个 close方法的调用者 才会调用 dismiss，如果是 Flutter页面控制器 才会从而触发 notifyWillDealloc 去销毁！
-             * 其他里面的 Flutter页面控制器 并不会自动执行 didMove(toParent: nil)，因此其他的根本没死去！
+             *【导航控制器dismiss方法会内存泄漏！！！！！！】
+             *【Flutter页面控制器】都是 FLBFlutterViewContainer 这个类
+             * FLBFlutterViewContainer 内部要调用 notifyWillDealloc 才能触发dart端去销毁页面，才会真正的死去
+             *【不执行 notifyWillDealloc 就死不了，flutter_boost只是把它隐藏了】
+             * 但是 notifyWillDealloc 是内部方法（只在.m文件），只在 dismiss 和 didMove(toParent: nil) 方法里面调用
+             * 可是这里只有这个【close方法的调用者】才会调用 dismiss，并且如果是【Flutter页面控制器】才会从而触发 notifyWillDealloc 去销毁！
+             * 然而导航控制器里面其他的【Flutter页面控制器】并不会自动执行 didMove(toParent: nil)，因此其他的vc根本没死去！
              * 解决方法：dismiss后手动调用其他的 Flutter页面控制器 的 didMove(toParent: nil) 或 perform(Selector(("notifyWillDealloc"))) 方法！
-             * 注意：notifyWillDealloc 不能重复调用！否则不能死干净！也就是说 dismiss 的这个不能再 didMove(toParent: nil) 了，FLBFlutterViewContainer 内部并没有做防重处理（垃圾）
+             * 注意：notifyWillDealloc 不能重复调用！否则不能死干净！也就是说 dismiss 的这个vc不能再调用 didMove(toParent: nil) 了，FLBFlutterViewContainer 内部并没有做防重处理（垃圾）
              */
             guard let navCtr = self.navigationController else { return }
             for vc in navCtr.viewControllers where vc != self && vc is FLBFlutterViewContainer {
